340abfa4d610632fc5340321df4a602dfb4cf39b
David Dai <david.dai@iag.com.au>
2017-09-14 13:41:39 +1000

    init commit.

---
+++ src/index.js
@@ -0,0 +1,98 @@
+import _ from 'lodash';
+
+const getProperty = (obj, fields) => fields.reduce((result, field) => result && result[field] ? result[field] : null, obj);
+
+const setProperty = (obj, fields, value) => fields.reduce((result, field, index) => {
+    if (index === fields.length - 1) {
+        result[field] = value;
+    } else if (!result[field]) {
+        result[field] = _.isInteger(fields[index + 1]) ? [] : {};
+    }
+    return result[field];
+}, obj);
+
+const getPaths = (obj, fields) => {
+    const paths = [];
+    const path = [];
+    let returnPaths = false;
+
+    fields.forEach((field) => {
+        if (returnPaths) {
+            return;
+        }
+        const nextPath = [...path, field];
+        const value = getProperty(obj, nextPath);
+        if (Array.isArray(value)) {
+            for (let i = 0; i < value.length; i += 1) {
+                const subPaths = getPaths(value[i], fields.slice(nextPath.length));
+                subPaths.forEach(subPath => paths.push([...nextPath, i, ...subPath]));
+            }
+            returnPaths = true;
+        } else if (!returnPaths){
+            path.push(field);
+        }
+    });
+    if (!returnPaths) {
+        paths.push(path);
+    }
+
+    return paths;
+};
+
+export default class Validation {
+    constructor(values) {
+        this.values = values;
+        this.errors = {};
+    }
+
+    getErrors() {
+        return this.errors;
+    }
+
+    hasError() {
+        return !_.isEmpty(this.errors);
+    }
+
+    validation(field, message, validate, againstNullOrUndefined) {
+        const props = field.split('.');
+        const paths = getPaths(this.values, props);
+
+        paths.forEach(path => {
+            const property = getProperty(this.values, path);
+            if (againstNullOrUndefined || property) {
+                if (!validate(property)) {
+                    setProperty(this.errors, path, message || 'property is invalid');
+                }
+            }
+        });
+
+        return this;
+    }
+
+    validEmail(field, message) {
+        return this.validation(field, message, value => /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value));
+    }
+
+    validInteger(field, message) {
+        return this.validation(field, message || 'invalid integer', _.isInteger);
+    }
+
+    maxLength(field, message, maxLength) {
+        return this.validation(field, message, value => value.length <= maxLength);
+    }
+
+    containsOnly(propList, message) {
+        if (!_.keys(this.values).every(key => propList.indexOf(key) >= 0)) {
+            this.errors.body = message || `body should only contain ${propList.join(', ')}`;
+        }
+        return this;
+    }
+
+    require(field, message) {
+        if (field === '/' && _.isEmpty(this.values)) {
+            this.errors.body = message || 'body is required but missing';
+            return this;
+        }
+        return this.validation(field, message || 'property is required but missing', value => !!value, true);
+    }
+}
---
+++ test/index.js
@@ -0,0 +1,146 @@
+import { expect } from 'chai';
+import Validation from '../src/index';
+
+describe('Validation', () => {
+    it('should give error when required field is missing', () => {
+        let values = { firstName: 'John', lastName: 'Smith' };
+        let errors = new Validation(values)
+            .require('lastName', 'last name is missing')
+            .getErrors();
+
+        expect(errors.lastName).to.be.undefined;
+
+        values = { firstName: 'John' };
+        errors = new Validation(values)
+            .require('lastName', 'last name is missing')
+            .getErrors();
+
+        expect(errors.lastName).to.equal('last name is missing');
+    });
+
+    it('should give error when length is over maxLength', () => {
+        let values = { firstName: 'John' };
+        let errors = new Validation(values)
+            .maxLength('firstName', 'firstName is too long', 4)
+            .getErrors();
+
+        expect(errors.firstName).to.be.undefined;
+
+        values = { firstName: 'John' };
+        errors = new Validation(values)
+            .maxLength('firstName', 'firstName is too long', 3)
+            .getErrors();
+
+        expect(errors.firstName).to.equal('firstName is too long');
+    });
+
+    it('should give error when email field is not email', () => {
+        let values = { email: 'john@test.com' };
+        let errors = new Validation(values)
+            .validEmail('email', 'invalid email')
+            .getErrors();
+
+        expect(errors.email).to.be.undefined;
+
+        values = { email: 'john@t.m' };
+        errors = new Validation(values)
+            .validEmail('email', 'invalid email')
+            .getErrors();
+
+        expect(errors.email).to.equal('invalid email');
+    });
+
+    it('should give error if the object contains unexpected property', () => {
+        let values = { firstName: 'John', email: 'john@test.com' };
+        let errors = new Validation(values)
+            .containsOnly(['firstName', 'email'])
+            .getErrors();
+
+        expect(errors.body).to.be.undefined;
+
+        values = { firstName: 'John', email: 'john@test.com', address: '123 Test St' };
+        errors = new Validation(values)
+            .containsOnly(['firstName', 'email'])
+            .getErrors();
+
+        expect(errors.body).to.equal('body should only contain firstName, email');
+    });
+
+    it('should validate nested property without arrays', () => {
+        let values = {
+            person: { firstName: 'John', lastName: 'Smith' }
+        };
+        let errors = new Validation(values)
+            .require('person.lastName')
+            .getErrors();
+        expect(errors).is.empty;
+
+        values = {
+            person: { firstName: 'John' }
+        };
+        errors = new Validation(values)
+            .require('person.lastName', 'last name is required')
+            .getErrors();
+        expect(errors.person.lastName).to.equal('last name is required');
+    });
+
+    it('should validate nested property with arrays', () => {
+        let values = {
+            persons: [
+                {
+                    firstName: 'John',
+                    lastName: 'Smith',
+                    hobbies: [
+                        { type: 'sport', value: 'soccer' },
+                        { type: 'sport', value: 'basketball' },
+                    ]
+                },
+                {
+                    firstName: 'Alex',
+                    lastName: 'Gordon',
+                    hobbies: [
+                        { value: 'chess' },
+                        { value: 'basketball' },
+                    ]
+                },
+            ]
+        };
+        let errors = new Validation(values)
+            .require('persons.hobbies.type', 'type is missing')
+            .getErrors();
+
+        expect(errors).to.deep.equal({
+            persons: [undefined, { hobbies: [{ type: 'type is missing' }, { type: 'type is missing' }] }]
+        });
+    });
+
+    it('should validate regardless of null or undefined values', () => {
+        let values = {
+            persons: [
+                {
+                    firstName: 'John',
+                    lastName: 'Smith',
+                    hobbies: [
+                        { type: 1, value: 'soccer' },
+                        { type: 'sport', value: 'basketball' },
+                    ]
+                },
+                {
+                    firstName: 'Alex',
+                    lastName: 'Gordon',
+                    hobbies: [
+                        { type: 'sport', value: 'chess' },
+                        { value: 'basketball' },
+                    ]
+                },
+            ]
+        };
+        let errors = new Validation(values)
+            .validInteger('persons.hobbies.type', 'should be integer')
+            .getErrors();
+
+        expect(errors).to.deep.equal({
+            persons: [{ hobbies: [undefined, { type: 'should be integer' }] }, { hobbies: [{ type: 'should be integer' }] }]
+        });
+    });
+});
